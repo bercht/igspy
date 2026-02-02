d9efeae006c971c62350bf6b64c6fd08b2657e97
Curt Bercht Neto <bercht@live.com>
2026-02-02 13:41:18 -0300

    Design: página de login minimalista e elegante

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+    
   # Página inicial pública
   root "home#index"
 
