5669beb400bf9d57d5a465355b2d7a8d90d11d22
Curt Bercht Neto <bercht@live.com>
2026-02-02 13:46:03 -0300

    Config: redirect para dashboard após login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,20 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Configurar layout por controller
+  layout :layout_by_resource
+
   protected
+
+  # Redirect após login - vai para o dashboard
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+
+  # Redirect após logout - vai para a home pública
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 
   def configure_permitted_parameters
     # Permitir API keys e preferência de chat na atualização de conta (edit/update)
@@ -17,4 +30,14 @@ class ApplicationController < ActionController::Base
     # Se você quiser permitir na criação também (sign_up), descomente a linha abaixo:
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:manus_api_key, :anthropic_api_key, :preferred_chat_api])
   end
-end 
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+end
