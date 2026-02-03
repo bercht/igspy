5ef305cb291abdd4926c75bc4f5c53e49bf397eb
Curt Bercht Neto <bercht@live.com>
2026-02-03 15:49:44 -0300

    bug corrijido

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,5 @@
-class WebhooksController < ApplicationController
+class WebhooksController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!
 
   def stripe
     payload = request.body.read
