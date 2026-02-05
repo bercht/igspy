635628280b2213c1fe80881506d6e7efb71263e8
Curt Bercht Neto <bercht@live.com>
2026-02-05 00:03:14 -0300

    site off

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_controller.default_url_options = { host: 'apps.curt.com.br', protocol: 'https' }
+  config.action_controller.asset_host = 'https://apps.curt.com.br'
 end
